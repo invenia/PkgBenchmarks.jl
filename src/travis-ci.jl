ac62e34ceaa29779559ea45777cb6b03fa5a0f79
Curtis Vogt <curtis.vogt@gmail.com>
2016-09-02 10:41:33 -0500

    Remove explicit add of BenchmarkTools

--- src/travis-ci.jl
+++ src/travis-ci.jl
@@ -21,7 +21,6 @@ function travis_benchmark(slug, token, pkg, baseline, candidate)
                     "julia -e 'Pkg.init(); symlink(pwd(), Pkg.dir(\"$pkg\")); Pkg.resolve(); Pkg.build(\"$pkg\");'",
                     # "julia -e 'Pkg.clone(pwd()); Pkg.build(\"$pkg\");'",
                     "julia -e 'Pkg.clone(\"https://github.com/invenia/PkgBenchmarks.jl\"); Pkg.checkout(\"PkgBenchmarks\", \"travis-ci\");'",  # Temporary
-                    "julia -e 'Pkg.add(\"BenchmarkTools\");'",  # Should be handled by a REQUIRE file
                 ],
                 "script" => [
                     # TODO: Cause failure upon finding any regression
