3a037cdb6836bd1c3e6a756df75c7bd97356bf5b
Curtis Vogt <curtis.vogt@gmail.com>
2016-09-02 12:37:21 -0500

    Additional dev comments

--- src/travis-ci.jl
+++ src/travis-ci.jl
@@ -1,5 +1,8 @@
 using Requests
 
+# TODO: Should probably move this outside of this repo as it forces us to install Requests
+# which isn't usually needed.
+
 function travis_benchmark(slug, token, pkg, baseline, candidate)
     slug = replace(slug, "/", "%2F")
 
@@ -18,11 +21,12 @@ function travis_benchmark(slug, token, pkg, baseline, candidate)
                 "install" => [
                     "git remote set-branches origin $baseline $candidate",
                     "if [[ -a .git/shallow ]]; then git fetch --unshallow; fi",
-                    "git branch $baseline origin/$baseline",
+                    "git branch $baseline origin/$baseline",  # Now both candidate and baseline are local branches
                     "julia -e 'Pkg.clone(pwd()); Pkg.build(\"$pkg\");'",
                     "julia -e 'Pkg.clone(\"https://github.com/invenia/PkgBenchmarks.jl\"); Pkg.checkout(\"PkgBenchmarks\", \"travis-ci\");'",  # Temporary
                 ],
                 "script" => [
+                    # Note: Due to using `Pkg.clone` the origin actually refers to local branches in the `pwd()` repo.
                     "julia -e 'using PkgBenchmarks, BenchmarkTools; a, b = benchmark(\"$pkg\", \"origin/$baseline\", \"origin/$candidate\"); j = judge(minimum(a), minimum(b)); println(leaves(j)); showall(a); showall(b); exit(!isempty(regressions(j)))'",
                 ],
                 "after_success" => "echo hi",  # TODO: find a better way to override "after_success"
